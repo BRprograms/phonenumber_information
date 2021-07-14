8b8e67394653311401316e47106daa9545ddd6a0
BRprograms <86615702+BRprograms@users.noreply.github.com>
2021-07-14 22:18:49 +0700

    Update phonenumberinfo.py

--- phonenumberinfo.py
+++ phonenumberinfo.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
-phone_number=phonenumbers.parse("+919323130355")
+phone_number=phonenumbers.parse("+6281317850827")
 print(geocoder.description_for_number(phone_number,'en'))
 print(carrier.name_for_number(phone_number,'en'))
